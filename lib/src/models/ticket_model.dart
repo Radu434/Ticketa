cd5db5cbd13823b62988095beff83bfea4f8726f
Pestesan Radu <radupestesan@yahoo.com>
2024-05-15 21:46:23 +0300

    Added ticket model

---
+++ lib/src/models/ticket_model.dart
@@ -0,0 +1,14 @@
+import 'dart:js_interop';
+
+class Ticket {
+  int _ticketId;
+  String _buyerId;
+  String _eventId;
+  double _price;
+  String _date;
+
+  Ticket(this._ticketId, this._buyerId, this._eventId, this._price, this._date);
+
+
+
+}
